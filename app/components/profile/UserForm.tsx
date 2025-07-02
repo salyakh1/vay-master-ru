5443e3280474a3fc374f6bb2dcf609d478935982
salyakh <saleh060p@gmail.com>
2025-07-02 05:33:01 +0300

    full reupload: повторная заливка после удаления старого репозитория

--- app/components/profile/UserForm.tsx
+++ app/components/profile/UserForm.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { useState } from 'react';
-import { User, UserRole, SocialLinks, Service, ServiceArea, GalleryImage } from '@/types/user';
+import { BaseUser, UserRole, SocialLinks, Service, ServiceArea, GalleryImage } from '@/types/user';
 import Image from 'next/image';
 import ServiceManager from './ServiceManager';
 import GalleryManager from './GalleryManager';
 import ProductManager from './ProductManager';
 
 interface UserFormProps {
-  initialData?: Partial<User>;
-  onSubmit: (data: Partial<User>) => Promise<void>;
+  initialData?: Partial<BaseUser>;
+  onSubmit: (data: Partial<BaseUser>) => Promise<void>;
 }
 
 type SocialNetwork = keyof SocialLinks;
